d076e27624127d743d892e0b4041f2010cbe35e7
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-14 15:26:48 +0300

    base class

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,10 @@ class BaseModel:
         kwargs: dictionary that contains info
     """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
         else:
             for key, values in kwargs.items():
@@ -45,6 +45,7 @@ class BaseModel:
         """
         new_dict = self.__dict__
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        dateformat = "%Y-%m-%dT %H:%M:%S.%f"
+        new_dict["created_at"] = self.created_at.strftime(dateformat)
+        new_dict["updated_at"] = self.updated_at.strftime(dateformat)
         return new_dict
