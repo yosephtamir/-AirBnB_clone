2da28e647076e211bbdc4b4916b2796aa7991d54
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-14 15:15:20 +0300

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,7 @@
 FileStorage module:
 """
 import json
-import models
+
 class FileStorage:
     """
      a class FileStorage that serializes instances to a JSON file and
@@ -16,33 +16,33 @@ class FileStorage:
         """
         This returns the dictionary __objects
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
         This method sets in __objects the obj with key <obj class name>.id
         """
         if obj is not None:
-            i = f"{obj.__class__.__name__}.{obj.id}"
-            self.__objects[i] = obj
+            FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
     def save(self):
         """
         This method serializes __objects to the JSON file (path: __file_path)
         """
-        new = {}
-        for i in self.__objects:
-            new[i] = self.__objects[i].to_dict()
-
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(new, file)
-
+        objectCopy  = FileStorage.__objects
+        for obj in objectCopy.keys():
+            new_dict = {obj: objectCopy[obj].to_dict()}
+  
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(new_dict, file)
+        
     def reload(self):
         """
         This method deserializes the JSON file to __objects
         """
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                self.__objects = json.load(file)
-        except Exception:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                content = json.load(file)
+                print(content)
+        except FileNotFoundError:
             return
