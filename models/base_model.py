7cf3290acb325b14b38d7b08b5b0cda3e9d11a51
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-14 15:30:28 +0300

    base class

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             for key, values in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
