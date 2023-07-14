cf1f688a07fa83b5be0f7817307a2843c8940b19
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-14 16:28:04 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,35 +16,19 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        dateformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, dateformat)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = val
         else:
             storage.new(self)
-
-    # def __init__(self, *args, **kwargs):
-    #     if not kwargs:
-    #         self.id = str(uuid.uuid4())
-    #         self.created_at = datetime.now()
-    #         self.updated_at = datetime.now()
-    #     else:
-    #         for key, values in kwargs.items():
-    #             if key == 'created_at' or key == 'updated_at':
-    #                 dateformat = "%Y-%m-%dT %H:%M:%S.%f"
-    #                 values = datetime.strptime(kwargs[key], dateformat)
-    #             if key != '__class__':
-    #                 setattr(self, key, values)
 
     def __str__(self):
         """return string presentation of class"""
@@ -61,7 +45,7 @@ class BaseModel:
         """return dictionary presentation of class
         created_at and updated_at: all are updated
         """
-        rdict = self.__dict__.copy()
+        rdict = self.__dict__
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
