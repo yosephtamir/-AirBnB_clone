4d31fe73c0b75574711d879cc3d50ff9998a1e6d
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-14 15:48:11 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,8 @@ class FileStorage:
         This method serializes __objects to the JSON file (path: __file_path)
         """
         objectCopy  = FileStorage.__objects  
-        with open(FileStorage.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file
+            new_dict = {}
             for obj in objectCopy.keys():
                 new_dict = {obj: objectCopy[obj].to_dict()}
             json.dump(new_dict, file)
