24dc4fcde526ebcde0c07275d59df1787576dc91
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-15 21:34:48 +0300

    user

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This Module is used to show users
+"""
+from base_model import BaseModel
+
+class User(BaseModel):
+    """
+    A user identfier class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization point
+        """
+        super().__init__(self, *args, **kwargs)
