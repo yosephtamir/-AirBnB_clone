1902f607152d08f63f5364990467f6e8cb5c1d3e
Eugene MUTUYIMANA <108212386+eugenesucess@users.noreply.github.com>
2023-07-16 09:30:44 +0300

    test file for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""Unittest of base_model file
+it is based on BaseModel class"""
 
+
+import unittest
+from models.base_model import  BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    def test_output_of_class_attributes(self):
+        obj = BaseModel()
+        obj.name = "Eugene"
+        obj.number = 89
+        obj.save()
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj.name, "Eugene")
+        self.assertEqual(obj.number, 89)
+        self.assertEqual(isinstance(obj.created_at, datetime), True)
+        self.assertEqual(isinstance(obj.updated_at, datetime), True)
+        self.assertEqual(type(obj_dict), dict)
+
+if __name__ == "__main__":
+    unittest.main()
