69a578d38d846c45937abd245cad0de50dc903f8
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-16 12:17:57 +0300

    city

---
+++ models/city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+City module
+"""
+from models import BaseModel
+
+
+class City(BaseModel):
+    """
+    This is a City class that inherits from base Model
+    """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        This is the initialization point of the City class
+        """
+        super().__init__(self, *args, **kwargs)
