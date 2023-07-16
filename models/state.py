ba1816774906961ef13972d027d4fe78433724e1
Yoseph Tamirat <yosephtamir@gmail.com>
2023-07-16 11:51:02 +0300

    state

---
+++ models/state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is used to hold the state
+"""
+from base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    This class inherits from Base Model
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        This is used to initialize the State class
+        """
+        super().__init__(*args, **kwargs)
